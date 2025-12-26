0b1799dbf01e3606a77ed0cd270dbcdfa9817afe
AhmadAdewumi <ahmadadewumi@gmail.com>
2025-12-26 21:25:00 +0100

    update: implement palindrome from striver's

---
+++ src/striversAtoZ/Palindrome.java
@@ -0,0 +1,21 @@
+package striversAtoZ;
+
+public class Palindrome {
+    static boolean isPalindrome(int n) {
+        if (n < 0 || (n % 10 == 0 && n != 0)) {
+            return false;
+        }
+
+        int revertedNumber = 0;
+        while (n > revertedNumber) {
+            revertedNumber = revertedNumber * 10 + n % 10; //-- build the reverted number by appending remainder
+            n = n / 10; //-- strip the last digit
+        }
+
+        return n == revertedNumber || n == revertedNumber / 10;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Is the number palindrome? : " + isPalindrome(4774));
+    }
+}
